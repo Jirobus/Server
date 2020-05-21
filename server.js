adb1542494fddb46730c7be0349962257284c7ab
DarthPigrum <darthpigrum@gmail.com>
2020-05-21 15:07:40 +0300

    Moved to entity based API

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const fs = require("fs");
 const path = require("path");
 const http = require("http");
 
-const api = new Map();
-const apiPath = "./api/";
+const entities = new Map();
+const entitiesPath = "./entities/";
 const httpError = (res, status, message) => {
   res.statusCode = status;
   res.end(message);
@@ -13,8 +13,8 @@ const httpError = (res, status, message) => {
 
 global.config = JSON.parse(fs.readFileSync("config.json"));
 
-fs.readdirSync(apiPath).forEach((name) => {
-  const filePath = apiPath + name;
+fs.readdirSync(entitiesPath).forEach((name) => {
+  const filePath = entitiesPath + name;
   const key = path.basename(filePath, ".js");
   try {
     const libPath = require.resolve(filePath);
@@ -23,29 +23,37 @@ fs.readdirSync(apiPath).forEach((name) => {
     return;
   }
   try {
-    const method = require(filePath);
-    api.set(key, method);
+    const entity = require(filePath);
+    entities.set(key, entity);
   } catch (e) {
-    api.delete(name);
+    entities.delete(name);
   }
 });
 
 http
   .createServer(async (req, res) => {
     const url = req.url === "/" ? "/index.html" : req.url;
-    const [first, second, ...args] = url.substring(1).split("/");
-    if (first === "api") {
-      const method = api.get(second);
-      try {
-        const result = await method(...args);
-        if (typeof result !== "string" && !(result instanceof Buffer)) {
+    const [type, name, action] = url.substring(1).split("/");
+    if (type === "api") {
+      const entity = entities.get(name);
+      const chunks = [];
+      req.on("data", (chunk) => chunks.push(chunk));
+      req.on("end", async () => {
+        try {
+          const result = await entity[action](JSON.parse(chunks.join("")));
+          if (typeof result === "object") {
+            res.end(JSON.stringify(result));
+          } else if (
+            typeof result !== "string" &&
+            !(result instanceof Buffer)
+          ) {
+            httpError(res, 500, "Server error");
+            return;
+          } else res.end(result);
+        } catch (e) {
           httpError(res, 500, "Server error");
-          return;
         }
-        res.end(result);
-      } catch (e) {
-        httpError(res, 500, "Server error");
-      }
+      });
     } else {
       const path = `./static${url}`;
       fs.exists(path, (exists) => {
