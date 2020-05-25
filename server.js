4b7beb69d71679c2d6f6b7a18b4a56ae4f524897
DarthPigrum <darthpigrum@gmail.com>
2020-05-25 11:40:44 +0300

    Server implementation

--- server.js
+++ server.js
@@ -1,65 +1,61 @@
 "use strict";
 
 const fs = require("fs");
-const path = require("path");
 const http = require("http");
+const mysql = require("mysql2/promise");
 
-const entities = new Map();
-const entitiesPath = "./entities/";
-const httpError = (res, status, message) => {
-  res.statusCode = status;
-  res.end(message);
-};
+const httpError = require("./utils/httpError");
+const loadEntities = require("./utils/loadEntities");
 
-global.config = JSON.parse(fs.readFileSync("config.json"));
+(async function main() {
+  const entities = {
+    public: new Map(),
+    private: new Map(),
+  };
+  loadEntities("./entities/public", entities.public);
+  loadEntities("./entities/private", entities.private);
 
-fs.readdirSync(entitiesPath).forEach((name) => {
-  const filePath = entitiesPath + name;
-  const key = path.basename(filePath, ".js");
-  try {
-    const libPath = require.resolve(filePath);
-    delete require.cache[libPath];
-  } catch (e) {
-    return;
-  }
-  try {
-    const entity = require(filePath);
-    entities.set(key, entity);
-  } catch (e) {
-    entities.delete(name);
-  }
-});
+  global.config = JSON.parse(fs.readFileSync("config.json"));
+  global.db = await mysql.createConnection(global.config.mysqlConfig);
 
-http
-  .createServer(async (req, res) => {
-    const url = req.url === "/" ? "/index.html" : req.url;
-    const [type, name, action] = url.substring(1).split("/");
-    if (type === "api") {
-      const entity = entities.get(name);
-      const chunks = [];
-      req.on("data", (chunk) => chunks.push(chunk));
-      req.on("end", async () => {
-        try {
-          const result = await entity[action](JSON.parse(chunks.join("")));
-          if (typeof result === "object") {
-            res.end(JSON.stringify(result));
-          } else if (
-            typeof result !== "string" &&
-            !(result instanceof Buffer)
-          ) {
+  http
+    .createServer(async (req, res) => {
+      const url = req.url === "/" ? "/index.html" : req.url;
+      const [endpoint, name, action] = url.substring(1).split("/");
+      if (
+        endpoint === "api" ||
+        global.config.privateApiEndpoints.includes(endpoint)
+      ) {
+        const entity = (endpoint === "api"
+          ? entities.public
+          : entities.private
+        ).get(name);
+        const chunks = [];
+        req.on("data", (chunk) => chunks.push(chunk));
+        req.on("end", async () => {
+          try {
+            const result = await entity[action](JSON.parse(chunks.join("")));
+            if (typeof result === "object") {
+              res.end(JSON.stringify(result));
+            } else if (
+              typeof result !== "string" &&
+              !(result instanceof Buffer)
+            ) {
+              httpError(res, 500, "Server error");
+              return;
+            } else res.end(result);
+          } catch (e) {
+            console.log(e);
             httpError(res, 500, "Server error");
-            return;
-          } else res.end(result);
-        } catch (e) {
-          httpError(res, 500, "Server error");
-        }
-      });
-    } else {
-      const path = `./static${url}`;
-      fs.exists(path, (exists) => {
-        if (exists) fs.createReadStream(path).pipe(res);
-        else httpError(res, 404, "File is not found");
-      });
-    }
-  })
-  .listen(global.config.port);
+          }
+        });
+      } else {
+        const path = `./static${url}`;
+        fs.exists(path, (exists) => {
+          if (exists) fs.createReadStream(path).pipe(res);
+          else httpError(res, 404, "File is not found");
+        });
+      }
+    })
+    .listen(global.config.port);
+})();
